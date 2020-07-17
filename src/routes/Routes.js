cb0dff0c564951a59cda0c1e0abeb0ab5553bc96
Ron Braha <Ron.Braha@icloud.com>
2020-07-17 21:50:06 +0300

    add lazy loading

--- src/routes/Routes.js
+++ src/routes/Routes.js
@@ -1,15 +1,18 @@
-import React from 'react'
+import React, { lazy, Suspense } from 'react'
 import { Switch, Route } from 'react-router-dom'
-import LandingPage from '../pages/LandingPage'
-import CatalogPage from '../pages/CatalogPage'
-import MovieDetailPage from '../pages/MovieDetailPage'
+
+const LandingPage = lazy(() => import('../pages/LandingPage'))
+const CatalogPage = lazy(() => import('../pages/CatalogPage'))
+const MovieDetailPage = lazy(() => import('../pages/MovieDetailPage'))
 
 export default function Routes() {
 	return (
-		<Switch>
-			<Route exact path='/' component={LandingPage} />
-			<Route path='/catalog' component={CatalogPage} />
-			<Route path='/movies/:id' component={MovieDetailPage} />
-		</Switch>
+		<Suspense fallback={<div>Loading...</div>}>
+			<Switch>
+				<Route exact path='/' component={LandingPage} />
+				<Route path='/catalog' component={CatalogPage} />
+				<Route path='/movies/:id' component={MovieDetailPage} />
+			</Switch>
+		</Suspense>
 	)
 }
