d79c01269702d72bdb50a05d551834d6420a3326
Ron Braha <ron.braha@icloud.com>
2020-09-29 14:55:16 +0300

    migrate layout to styled

--- client/src/components/Layout/Layout.tsx
+++ client/src/components/Layout/Layout.tsx
@@ -1,18 +1,14 @@
-import React, { ReactChild } from "react"
+import React, { PropsWithChildren } from "react"
 
-import classes from "./Layout.module.css"
 import Logo from "../Logo/Logo"
 import Navbar from "../Navbar/Navbar"
 import Search from "../Search/Search"
 import { useLocation } from "react-router-dom"
 import useSearch from "../../hooks/useSearch"
 import useCurrentWidth from "../../hooks/useCurrentWidth"
+import styled from "styled-components"
 
-interface Props {
-  children: ReactChild
-}
-
-export default function Layout({ children }: Props) {
+export default function Layout({ children }: PropsWithChildren<{}>) {
   const { pathname } = useLocation()
   const { displaySearch, handleChange, searchValue, toggleSearch } = useSearch()
   const width = useCurrentWidth()
@@ -20,8 +16,8 @@ export default function Layout({ children }: Props) {
   const displayNavbar = (!displaySearch || lg) && pathname !== "/"
 
   return (
-    <div className={classes.Layout}>
-      <div className={classes.Header}>
+    <Container>
+      <Header>
         <Logo />
         {displayNavbar && <Navbar />}
         {pathname === "/catalog" && (
@@ -32,8 +28,32 @@ export default function Layout({ children }: Props) {
             value={searchValue}
           />
         )}
-      </div>
-      <main className={classes.Main}>{children}</main>
-    </div>
+      </Header>
+      <main>{children}</main>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  background-color: #141414;
+  height: 100vh;
+
+  main {
+    margin: 0 auto;
+    max-width: 1320px;
+    overflow: hidden;
+  }
+`
+
+const Header = styled.div`
+  margin: 0 auto;
+  display: flex;
+  position: fixed;
+  background-color: #0f0f0f;
+  align-items: flex-end;
+  justify-content: flex-start;
+  width: 100%;
+  height: 50px;
+  padding: 5px;
+  z-index: 1;
+`
